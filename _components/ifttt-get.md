282f4eb5ea4c83236710a9dc5c43726fe097923f
Sayanee <sayanee@gmail.com>
2019-05-07 16:01:35 +0800

    Add component on sending a GET request to IFTTT with WeMos

---
+++ _components/ifttt-get.md
@@ -0,0 +1,13 @@
+---
+layout: components
+title: GET request to IFTTT
+description: Send a GET request to IFTTT periodically with WeMos
+code: ifttt-get/ifttt-get.ino
+hardware:
+  - esp8266
+  - WeMos
+  - IFTTT
+  -
+---
+
+Send a GET request to IFTTT periodically with WeMos after connecting it to the WiFi.
---
+++ _components/ifttt-get/ifttt-get.ino
@@ -0,0 +1,70 @@
+#include <ESP8266WiFi.h>
+
+extern "C" {
+  #include "user_interface.h"
+}
+
+// Edit "secret" ssid and password below
+const char* ssid = "secret";
+const char* password = "secret";
+
+// Set IFTTT Key from https://ifttt.com/services/maker_webhooks/settings
+String iftttKey = "secret";
+const char* host = "maker.ifttt.com";
+const int httpsPort = 443;
+
+void setup() {
+  Serial.begin(115200);
+  connectWiFi();
+}
+
+void loop() {
+  sendToIFTTT();
+  delay(10000); // every 10 seconds
+}
+
+void connectWiFi() {
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("[INFO] WiFi connected with IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void sendToIFTTT() {
+  WiFiClientSecure client;
+
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("[ERROR] Connection failed");
+    return;
+  }
+
+  String url = "/trigger/read_humidity/with/key/" + iftttKey;
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "User-Agent: ESP8266\r\n" +
+               "Connection: close\r\n\r\n");
+
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println("[ERROR] Client Timeout!");
+      return;
+    }
+  }
+
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  client.stop();
+  Serial.println("");
+  Serial.println("[INFO] Closing connection");
+  return;
+}
