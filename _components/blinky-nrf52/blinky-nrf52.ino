18f5086383027e1fd59833844399a51fd87a4c15
Sayanee <sayanee@gmail.com>
2019-05-15 13:53:20 +0800

    Integrate cpplint

--- _components/blinky-nrf52/blinky-nrf52.ino
+++ _components/blinky-nrf52/blinky-nrf52.ino
@@ -3,8 +3,11 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);                       // wait for 2 seconds
+  // turn the LED ON (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
 }
