f50bd5f8686db264c3491ea9d4109c63e5801fb1
Sayanee <sayanee@gmail.com>
2019-06-03 12:37:59 +0800

    Amend to #define constant

--- _components/max-sleep-ping/max-sleep-ping.ino
+++ _components/max-sleep-ping/max-sleep-ping.ino
@@ -11,6 +11,9 @@ String iftttKey = "secret";
 const char* host = "maker.ifttt.com";
 const int httpPort = 80;
 
+// Wakeup every 10,800,000,000 µs = 10800e6 = 10,800s = 3 hours
+#define SLEEP_INTERVAL_DURATION  10800e6
+
 void setup() {
   Serial.begin(115200);
   Serial.println("");
@@ -22,11 +25,12 @@ void setup() {
   blinkLED();
   sendToIFTTT();
 
-  // ESP.deepSleep(ESP.deepSleepMax());  // Wakeup every 3h 45min
+  // Wakeup every 3h 45min
+  // But this only makes it wakeup periodically for 3 times only, not infinitely
+  // ESP.deepSleep(ESP.deepSleepMax());
 
   Serial.println("[INFO] Going to sleep for 3 hours...");
-  // Wakeup every 10,800,000,000 µs = 10,800s = 3 hours
-  ESP.deepSleep((uint64_t)10800000000);
+  ESP.deepSleep(SLEEP_INTERVAL_DURATION, WAKE_RF_DEFAULT);
 }
 
 void loop() {}
