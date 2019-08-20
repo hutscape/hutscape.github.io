555d330c8d49d12f30eafbf1084c6ef5713685e2
Sayanee <sayanee@gmail.com>
2019-08-20 14:59:36 +0800

    Add serial print code for M0

--- _components/blinky-m0/blinky-m0.ino
+++ _components/blinky-m0/blinky-m0.ino
@@ -1,12 +1,19 @@
-#define LED 13 // Pin 13 is connected to an on-board Blue LED
+#define LED 13  // Pin 13 is connected to an on-board Blue LED
 
 void setup() {
   pinMode(LED, OUTPUT);
+
+  SerialUSB.begin(115200);
+  delay(100);
+  SerialUSB.println("Start!");
 }
 
 void loop() {
+  SerialUSB.println("HIGH");
   digitalWrite(LED, HIGH);
   delay(500);
+
+  SerialUSB.println("LOW");
   digitalWrite(LED, LOW);
   delay(500);
 }
