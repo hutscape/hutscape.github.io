014e746bf8f945f8b04eb9db608566f643078523
Sayanee <sayanee@gmail.com>
2019-12-03 11:18:29 +0800

    refactor: use openocd directly for flashing SAMD21G

--- _components/swd-programming-samd21g/swd-programming-samd21g.sh
+++ _components/swd-programming-samd21g/swd-programming-samd21g.sh
@@ -5,15 +5,10 @@ $ xcode-select -p # ensure XCocde CLI is installed
 $ brew install openocd
 $ openocd --help # ensure openocd is installed
 
-$ git clone git@github.com:adafruit/Adafruit_Adalink.git
-$ cd Adafruit_Adalink
-$ sudo python setup.py develop
-$ adalink atsamd21g18 --help # ensure Adalink is installed
-
 $ git clone git@github.com:arduino/ArduinoCore-samd.git
 $ cd ArduinoCore-samd/bootloaders/zero
 $ make
 $ ls bootloaders/zero/samd21_sam_ba.hex # ensure *.hex file is created
 
 $ # flash the Arduino bootloader!
-$ adalink -v atsamd21g18 -p stlink -h {PATH_TO_HEX_FILE}/ArduinoCore-samd/bootloaders/zero/samd21_sam_ba.hex
+$ openocd -f interface/stlink-v2.cfg -c "set CHIPNAME at91samd21g18; set ENDIAN little; set CPUTAPID 0x0bc11477; source [find target/at91samdXX.cfg]" -c "init" -c "reset init" -c "load_image {/path/to/ArduinoCore-samd/bootloaders/zero/samd21_sam_ba.hex} 0 ihex" -c "verify_image {/path/to/ArduinoCore-samd/bootloaders/zero/samd21_sam_ba.hex} 0 ihex" -c "reset run" -c "exit"
