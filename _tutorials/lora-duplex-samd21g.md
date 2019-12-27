c3175946c87be463216d5d17b05d83d9f7fcfb7a
Sayanee <sayanee@gmail.com>
2019-12-27 17:04:48 +0800

    docs: add tutorial for running arduino lora firmware with samd21g

---
+++ _tutorials/lora-duplex-samd21g.md
@@ -0,0 +1,25 @@
+---
+layout: tutorials
+title: LoRa Duplex SAMD21G
+hardware:
+  - samd21g
+  - m0
+  - lora
+  - duplex
+references:
+  - name: RobotDyn M0 pinout diagram
+    url: https://robotdyn.com/pub/media/0G-00005516==SAMD21-MINI/DOCS/PINOUT==0G-00005516==SAMD21-MINI.jpg
+  - name: RobotDyn M0 schematic
+    url: https://robotdyn.com/pub/media/0G-00005516==SAMD21-MINI/DOCS/Schematic==0G-00005516==SAMD21-MINI.pdf
+  - name: Arduino LoRa firmware
+    url: https://github.com/sandeepmistry/arduino-LoRa
+  - name: Ardino LoRa Duplex example code
+    url: https://github.com/sandeepmistry/arduino-LoRa/blob/master/examples/LoRaDuplex/LoRaDuplex.ino
+prerequisites:
+  - name: LoRa duplex A
+    url: ./lora-duplex-a
+  - name: LoRa duplex B
+    url: ./lora-duplex-b
+---
+
+Send and receive at the same time from another node! Use this code with [duplex b](lora-duplex-b).
---
+++ _tutorials/lora-duplex-samd21g/lora-duplex-samd21g.ino
@@ -0,0 +1,79 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+const int csPin = 7;
+const int resetPin = 6;
+const int irqPin = 1;
+
+byte localAddress = 0xAA;
+byte destinationAddress = 0xBB;
+long lastSendTime = 0;
+int interval = 2000;
+int count = 0;
+
+void setup() {
+  SerialUSB.begin(9600);
+  while (!SerialUSB) { }
+  delay(100);
+  SerialUSB.println("Start LoRa duplex");
+
+  LoRa.setPins(csPin, resetPin, irqPin);
+
+  if (!LoRa.begin(433E6)) {
+    SerialUSB.println("LoRa init failed. Check your connections.");
+    while (true) {}
+  }
+}
+
+void loop() {
+  if (millis() - lastSendTime > interval) {
+    String sensorData = String(count++);
+    sendMessage(sensorData);
+
+    SerialUSB.print("Sending data " + sensorData);
+    SerialUSB.print(" from 0x" + String(localAddress, HEX));
+    SerialUSB.println(" to 0x" + String(destinationAddress, HEX));
+
+    lastSendTime = millis();
+    interval = random(2000) + 1000;
+  }
+
+  receiveMessage(LoRa.parsePacket());
+}
+
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();
+  LoRa.write(destinationAddress);
+  LoRa.write(localAddress);
+  LoRa.write(outgoing.length());
+  LoRa.print(outgoing);
+  LoRa.endPacket();
+}
+
+void receiveMessage(int packetSize) {
+  if (packetSize == 0) return;
+
+  int recipient = LoRa.read();
+  byte sender = LoRa.read();
+  byte incomingLength = LoRa.read();
+
+  String incoming = "";
+
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length()) {
+    SerialUSB.println("Error: Message length does not match length");
+    return;
+  }
+
+  if (recipient != localAddress) {
+    SerialUSB.println("Error: Recipient address does not match local address");
+    return;
+  }
+
+  SerialUSB.print("Received data " + incoming);
+  SerialUSB.print(" from 0x" + String(sender, HEX));
+  SerialUSB.println(" to 0x" + String(recipient, HEX));
+}
