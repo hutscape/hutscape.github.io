599aa057f3acb817659f875f2962326776f36eb7
Sayanee <sayanee@gmail.com>
2020-07-01 16:44:28 +0800

    refactor: improve periodic wakeup with samd21g

--- _tutorials/periodic-wakeup/periodic-wakeup.ino
+++ _tutorials/periodic-wakeup/periodic-wakeup.ino
@@ -1,23 +1,25 @@
 #include <ArduinoLowPower.h>
+#define LED 13
 
 void setup() {
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+
   // Serial1 UART is used because the USB does not work after wakeup
   Serial1.begin(9600);
-  while (!Serial1) {}
+  // while (!Serial1) {}
   delay(1000);
 
   Serial1.println("Start of program");
+  Serial1.println("Fast blink 10 times");
   blink(10, 200);
-
-  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  Serial1.println("Blink 10 times");
+  Serial1.println("Slow blink 10 times");
   blink(10, 1000);
 
   Serial1.println("Going to sleep for 10 seconds...");
-
   LowPower.sleep(10000);  // in milliseconds
 
   Serial1.println("\n\nAwake");
@@ -28,10 +30,10 @@ void loop() {
 
 void blink(int times, int duration) {
   for (int i = 0; i < times; i++) {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED, LOW);
     delay(duration);
 
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED, HIGH);
     delay(duration);
   }
 }
