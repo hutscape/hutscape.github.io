d305d8fb5dd0efc03a9976f7aa876ce92d3c220c
Sayanee <sayanee@gmail.com>
2020-10-11 17:21:50 +0800

    feat: add example of webusb for sending an array

--- _tutorials/webusb-send/webusb-send.ino
+++ _tutorials/webusb-send/webusb-send.ino
@@ -2,6 +2,7 @@
 
 // Modified from example https://webusb.github.io/arduino/demos/console
 WebUSB WebUSBSerial(1, "webusb.github.io/arduino/demos/console");
+uint16_t irCode[4] = {3446, 1716, 416, 1310};
 
 void setup() {
   WebUSBSerial.begin(9600);
@@ -13,5 +14,12 @@ void loop() {
   WebUSBSerial.write("Hello world to the browser!");
   WebUSBSerial.flush();
 
+  writeWebUSB((const uint8_t *)irCode, 8);
+
   delay(2000);
+}
+
+void writeWebUSB(const uint8_t *buffer, int size) {
+    WebUSBSerial.write((const uint8_t *)buffer, size);
+    WebUSBSerial.flush();
 }
