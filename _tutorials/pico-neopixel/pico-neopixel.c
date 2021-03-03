d9aa67a537daa7ee146372f58609854d4e64163d
Sayanee <sayanee@gmail.com>
2021-03-03 16:18:04 +0800

    fix: remove cpplint errors

--- _tutorials/pico-neopixel/pico-neopixel.c
+++ _tutorials/pico-neopixel/pico-neopixel.c
@@ -12,7 +12,7 @@ static inline void put_pixel(uint32_t pixel_grb) {
   pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
 }
 
-static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b){
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
   return ((uint32_t)(r) << 8) |
          ((uint32_t)(g) << 16) |
          (uint32_t)(b);
@@ -33,26 +33,26 @@ int main() {
 
     // OPTION A: uncomment sleep_ms(500) and view the individual colors
     // OPTION B: comment sleep_ms(500) and view the pixels cascading colors
-    put_pixel(urgb_u32(0xff, 0, 0)); // Red
+    put_pixel(urgb_u32(0xff, 0, 0));  // Red
     sleep_ms(500);
-    put_pixel(urgb_u32(0, 0xff, 0)); // Blue
+    put_pixel(urgb_u32(0, 0xff, 0));  // Blue
     sleep_ms(500);
-    put_pixel(urgb_u32(0, 0, 0xff)); // Green
+    put_pixel(urgb_u32(0, 0, 0xff));  // Green
     sleep_ms(500);
-    put_pixel(urgb_u32(0xff, 0xff, 0)); // Purple
+    put_pixel(urgb_u32(0xff, 0xff, 0));  // Purple
     sleep_ms(500);
-    put_pixel(urgb_u32(0, 0xff, 0xff)); // Cyan
+    put_pixel(urgb_u32(0, 0xff, 0xff));  // Cyan
     sleep_ms(500);
-    put_pixel(urgb_u32(0xff, 0xff, 0xff)); // White
+    put_pixel(urgb_u32(0xff, 0xff, 0xff));  // White
     sleep_ms(500);
-    put_pixel(urgb_u32(0, 0, 0)); // Black or off
+    put_pixel(urgb_u32(0, 0, 0));  // Black or off
     sleep_ms(500);
 
     sleep_ms(1000);
     // Clear all pixels
     for (int i = 0; i <= 60; i++) {
-      put_pixel(urgb_u32(0, 0, 0)); // Black or off
+      put_pixel(urgb_u32(0, 0, 0));  // Black or off
     }
     sleep_ms(1000);
-  };
+  }
 }
