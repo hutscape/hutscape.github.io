50b22cd6f7d7761d53457cd083980993ff143122
Sayanee <sayanee@gmail.com>
2022-09-22 09:46:13 +0800

    refactor: rename hello-world to hello

--- _tutorials/hello-esp32s3/hello-esp32s3.ino
+++ _tutorials/hello-esp32s3/hello-esp32s3.ino
@@ -1,6 +1,6 @@
 void setup() {
   delay(500);
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(500);
   Serial.println("\n\n================================");
   Serial.print("Chip Model: ");
